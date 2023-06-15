af052b34c0e3aae9884dd397d3f5fe83a152bc26
TimbukTwoTwos <104961053+TimbukTwoTwos@users.noreply.github.com>
2023-06-15 12:14:39 +1000

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -11,8 +11,8 @@ model.load_model('catboost_model.json')
 @st.cache
 
 # Define the prediction function
-def predict(state, totalOrderCount, 30DayOrderCount, 90DayOrderCount, 180DayOrderCount, 365DayOrderCount, countOfDogFood, countOfCatFood, daysSinceLastOrder, groupLoyaltyCustomer, countOfItemID, minBagSize, maxBagSize, avgBagSize, mostFrequentCategoryValue, mostFrequentSubCategoryValue, daysBetweenOrderAVG, daysBetweenOrderSTD, averageRevenue, latestTransactionRevenue):
-    prediction = model.predict(pd.DataFrame([[state, totalOrderCount, 30DayOrderCount, 90DayOrderCount, 180DayOrderCount, 365DayOrderCount, countOfDogFood, countOfCatFood, daysSinceLastOrder, groupLoyaltyCustomer, countOfItemID, minBagSize, maxBagSize, avgBagSize, mostFrequentCategoryValue, mostFrequentSubCategoryValue,
+def predict(state, totalOrderCount, thirtyDayOrderCount, ninetyDayOrderCount, oneEightyDayOrderCount, ThreeSixFiveDayOrderCount, countOfDogFood, countOfCatFood, daysSinceLastOrder, groupLoyaltyCustomer, countOfItemID, minBagSize, maxBagSize, avgBagSize, mostFrequentCategoryValue, mostFrequentSubCategoryValue, daysBetweenOrderAVG, daysBetweenOrderSTD, averageRevenue, latestTransactionRevenue):
+    prediction = model.predict(pd.DataFrame([[state, totalOrderCount, thirtyDayOrderCount, ninetyDayOrderCount, oneEightyDayOrderCount, ThreeSixFiveDayOrderCount, countOfDogFood, countOfCatFood, daysSinceLastOrder, groupLoyaltyCustomer, countOfItemID, minBagSize, maxBagSize, avgBagSize, mostFrequentCategoryValue, mostFrequentSubCategoryValue,
            daysBetweenOrderAVG, daysBetweenOrderSTD, averageRevenue, latestTransactionRevenue]], columns=['State', 'totalOrderCount', '30DayOrderCount', '90DayOrderCount', '180DayOrderCount', '365DayOrderCount', 'countOfDogFood', 'countOfCatFood', 'daysSinceLastOrder', 'groupLoyaltyCustomer', 'countOfItemID', 'minBagSize', 'maxBagSize', 'avgBagSize', 'mostFrequentCategoryValue', 'mostFrequentSubCategoryValue',
            'daysBetweenOrderAVG', 'daysBetweenOrderSTD', 'averageRevenue', 'latestTransactionRevenue']))
     return prediction
@@ -22,10 +22,10 @@ st.title('Retail Propensity to Purchase')
 st.header('Enter the feature values of the customer:')
 state = st.selectbox('State:', ['NSW', 'QLD', 'VIC', 'WA', 'SA', 'NT', 'ACT', 'TAS'])
 totalOrderCount = st.number_input('Total Order Counnt:', min_value=0.1, max_value=100.0, value=1.0)
-30DayOrderCount = st.number_input('Total Order Counnt:', min_value=0.1, max_value=100.0, value=1.0)
-90DayOrderCount = st.number_input('Total Order Counnt:', min_value=0.1, max_value=100.0, value=1.0)
-180DayOrderCount = st.number_input('Total Order Counnt:', min_value=0.1, max_value=100.0, value=1.0)
-365DayOrderCount = st.number_input('Total Order Counnt:', min_value=0.1, max_value=100.0, value=1.0)
+thirtyDayOrderCount = st.number_input('Total Order Counnt:', min_value=0.1, max_value=100.0, value=1.0)
+ninetyDayOrderCount = st.number_input('Total Order Counnt:', min_value=0.1, max_value=100.0, value=1.0)
+oneEightyDayOrderCount = st.number_input('Total Order Counnt:', min_value=0.1, max_value=100.0, value=1.0)
+ThreeSixFiveDayOrderCount = st.number_input('Total Order Counnt:', min_value=0.1, max_value=100.0, value=1.0)
 countOfDogFood = st.number_input('Total Order Counnt:', min_value=0.1, max_value=100.0, value=1.0)
 
 
